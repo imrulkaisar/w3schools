3d3eb2c603c1e73b593e118e9d1666ce2ec85538
Imrul-Kaisar <imrulkaisar3@gmail.com>
2019-10-29 12:01:51 +0600

    JS Comparisons

---
+++ js/comparisons.js
@@ -0,0 +1,16 @@
+ /* 
+  * JS Comparisons
+  * */
+
+/* * 
+ * @ Conditional (Ternary) Operator
+ * @ Syntax: variableName = (condition) ? tureValue : falseValue;
+ */
+
+let voteable = ( age < 18 ) ? "Too young" : "Old enough";
+
+
+
+
+
+
