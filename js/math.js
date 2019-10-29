e2429e26623ee91754eb5c598dd88890d0103234
Imrul-Kaisar <imrulkaisar3@gmail.com>
2019-10-29 10:23:48 +0600

    JS Math

---
+++ js/math.js
@@ -0,0 +1,56 @@
+/* 
+ * Math Object
+ * */
+
+var x;
+Math.round(x) // return the value of x rounded to its nearest integer
+Math.round(4.7); // 5
+Math.round(4.4); // 4
+
+
+Math.pow(x, y); // return the value of x to the power of y; 
+Math.pow(8, 2); // 64
+
+
+Math.sqrt(x) // return the square root of x;
+Math.sqrt(64); // 8
+
+Math.abs(x) // return the absolute (positive) value of x; 
+
+Math.ceil(x); // return the value of x rounded up to its nearsest integer
+Math.ceil(4.4); // 5
+
+Math.floor(x) // return the value of x rounded down to its nearest interger
+Math.floor(4.7) // 4
+
+Math.sin(x) // return the sine (a value between -1 and 1) of the angle x (radians)
+Math.sin(90 * Math.PI/180) // 1 (90 is in degrees)
+
+Math.cos(x) // return the cosine (a value between -1 and 1) of the angle x (radians)
+
+Math.min(x, y, z) // return the lowest value of the arguments 
+Math.max(x, y, z) // return the highest value of the arguments 
+
+Math.random() // return the random number between 0 and 1
+
+/* 
+ * Math Propertices (Constants)
+ * */
+
+Math.E 		// returns Euler's number
+Math.PI 	// returns PI
+Math.SQRT2	// returns the square root of 2
+Math.SQRT1_2	// returns the square root of 1/2
+Math.LN2 	// returns the natural logarithm of 2
+Math.LN10 	// returns the natural logarithm of 10
+Math.LOG2E	// returns base 2 logarithm of E
+Math.lOG10E 	// returns base 10 logarithm of E
+
+
+Math.tan(x) 
+Math.atan(x) 
+Math.atan2(y,x) // returns the arctangent of the quotient of its arguments
+Math.log(x)  	// returns the natural logarithm of x (base E)
+
+
+
