1d8a404b1768b25d453c356a71e676636fae19f2
Imrul-Kaisar <imrulkaisar3@gmail.com>
2019-10-31 21:59:34 +0600

    JavaScript Style Guide and

---
+++ js/style-guide-and-coding-conventions.js
@@ -0,0 +1,47 @@
+/* 
+ * JavaScript Style Guide and Coding Conventions
+ * -----------------------------------------------
+ *
+ * */
+
+
+/*
+ * Common Rules 
+ * ----------------------------------
+ * Use camelCase for identifier names (variables and functions)
+ * All names start with a letter
+ * Always put spaces around operators ( = + - * / ) and after commas
+ * always use 2 spaces for indentation of code blocks. Don't use tabs for indentation
+ * Always end a simple statement with a semicolon
+ *
+ * General rules for complex statements
+ * --------------------------------------------
+ * Put the opening bracket at the end of the first line.
+ * Use one space before the opening bracket.
+ * Put the closing bracket on a new line without leading spaces.
+ * Don't end a complex statement with a semicolon
+ * ----------------------------------------------
+ *
+ * Object Rules
+ * ------------------------------
+ * Please the openingbracket on the same line as the object name. 
+ * Use colon plus one space between each property and its value. 
+ * Use quotes around string values, not around numeric values. 
+ * Don't add a comma after the last property-value pair. 
+ * Place the closing bracket on a new line, without leading spaces. 
+ * Always end an object definition with a semicolon. 
+ * --------------------------------------------------
+ *
+ * For readability, avoid lines longer than 80 characters.
+ * Always use the same naming conention for all your code.
+ *
+ * Naming Conventions 
+ * ----------------------------------------------
+ * Variable and function names written as camelCase
+ * Global variables written in UPPERCASE 
+ * Constants written in UPPERCASE
+ * Hyphens are not allowed in JavaScript names
+ * */
+
+
+
